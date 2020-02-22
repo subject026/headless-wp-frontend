8aaac1259ed7a666d4189a2ff570b8c513d2b5d5
subject026 <sudobuildspeakers@gmail.com>
2020-02-22 21:35:11 +0000

    commited built site

---
+++ public/component---src-pages-additional-page-js-4580b1254687e3c1933c.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[3],{"9uSa":function(e,t,a){"use strict";a.r(t);var n=a("q1tI"),o=a.n(n),l=a("Wbzz"),c=a("Bl7J"),r=a("vrFN");t.default=function(){return o.a.createElement(c.a,null,o.a.createElement(r.a,{title:"Page two"}),o.a.createElement("h1",null,"Woop Woop"),o.a.createElement("p",null,"Welcome to page 2"),o.a.createElement(l.Link,{to:"/"},"Go back to the homepage"))}}}]);
+//# sourceMappingURL=component---src-pages-additional-page-js-4580b1254687e3c1933c.js.map
